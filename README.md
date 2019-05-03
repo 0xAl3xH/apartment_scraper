76cc01e970836b264bc3ad64bf27cdd32e13a640
Alex Huang <alexh95@mit.edu>
2019-05-02 20:01:26 -0400

    Get SMS working

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Scraper to get alerts for available listings on Church Corner apartments website.
+## Scraper to get available listings on Church Corner apartments website and send SMS/Email alerts
+
+This script should be called with scrapy's scraper shell and will look for new listings. Once it finds a new listing within a specified time range it will use Twilio's API to send a text alert. All sensitive configuration should be done in `config.py` with the form:
+
+    # config.py
+    config = {
+        twilio_sid = xxxxxxx,
+        sensitive_info = zzzzzzz,
+        ...
+    }
+
+The script can be called periodically as a cron job or something similar.
+
+
--- scraper.py
+++ scraper.py
@@ -1,15 +1,36 @@
+from config import config
+
+TW_SID = config["tw_sid"]
+TW_TOKEN = config["tw_token"]
+TW_NUMBER = config["tw_number"]
+NUMBERS = config["numbers"]
+
 import scrapy
 from scrapy.crawler import CrawlerProcess
+
+from twilio.rest import Client
+client = Client(TW_SID, TW_TOKEN)
 
 num_listings = 0
 
 class ApartmentSpider(scrapy.Spider):
     name = "apartment_spider"
     start_urls = ["https://www.equityapartments.com/boston/central-square/church-corner-apartments##unit-availability-tile"]
+
+    # helper method to send an alert via SMS or email
+    @staticmethod
+    def alert(listing):
+        for number in NUMBERS:
+            client.messages.create( 
+                    body = "A listing at Church Corner Apartments for " + listing["Availability"] + " with rent " + listing["Price"] + " has been posted! ",
+                from_ = TW_NUMBER,
+                to = number
+            )
+
     # parases the response 
     def parse(self, res):
         global num_listings
-        #use CSS selector
+        #get the listings, which are in li elements
         SELECTOR = '#bedroom-type-2 ul li'
         
         for listing in res.css(SELECTOR):
@@ -17,8 +38,10 @@ class ApartmentSpider(scrapy.Spider):
             ROW_SELECTOR = '.row .specs p'
             for row in listing.css(ROW_SELECTOR):
                 PRICE_SELECTOR = '.pricing ::text'
+                # look for price if not found yet
                 if price is None:
                     price = row.css(PRICE_SELECTOR).extract_first()
+                # get inner HTML text
                 text = row.xpath('text()').extract()
                 for keyword in text:
                     # NOTE: assumes keyword will look like "Availability month/day/year"
@@ -26,7 +49,9 @@ class ApartmentSpider(scrapy.Spider):
                         num_listings += 1
                         # get the date string that should be in the form month/day/year
                         date_string = keyword.strip().split()[1]
-                        yield {"Availability": keyword.strip().split()[1], "Price":price}
+                        listing_result = {"Availability": keyword.strip().split()[1], "Price":price}
+                        ApartmentSpider.alert(listing_result)
+                        yield listing_result
 
 if __name__ == "__main__":
     process = CrawlerProcess({
